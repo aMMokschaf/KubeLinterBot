7960c0f631935887674872659811be1766bcac06
Marcel Singer <marcel.singer@hs-bochum.de>
2021-01-13 18:33:32 +0100

    Downloaded Files are removed after linting.

--- internal/handleresult/handle_result.go
+++ internal/handleresult/handle_result.go
@@ -2,6 +2,8 @@ package handleresult
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 )
 
 //TODO: Doc, ausprogrammieren
@@ -9,5 +11,30 @@ func HandleResult() {
 	fmt.Println("Entering HandleResult")
 	//exit code 1: comment
 	//exit code 0: no comment
-	//delete all downloaded files rm rf
+	err := removeDownloadedFiles("./downloadedYaml/")
+	if err != nil {
+		fmt.Println("Error while removing files.")
+	} else {
+		fmt.Println("Files removed.")
+	}
+}
+
+func removeDownloadedFiles(dir string) error {
+	fmt.Println("Entering removeDownloadedFiles()")
+	d, err := os.Open(dir)
+	if err != nil {
+		return err
+	}
+	defer d.Close()
+	names, err := d.Readdirnames(-1)
+	if err != nil {
+		return err
+	}
+	for _, name := range names {
+		err = os.RemoveAll(filepath.Join(dir, name))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
