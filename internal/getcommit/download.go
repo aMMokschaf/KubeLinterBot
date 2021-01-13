c485c9efc553ae700b93a96434b2f266b867fb8a
Marcel Singer <marcel.singer@hs-bochum.de>
2021-01-13 16:58:28 +0100

    Removed typo and old code

--- internal/getcommit/download.go
+++ internal/getcommit/download.go
@@ -49,33 +49,3 @@ func DownloadCommit(token string, username string, reponame string, commitSha st
 
 	return downloadStatus
 }
-
-/*
-func DownloadFile(url string, filename string) error {
-	//TODO implement subfolders
-	fmt.Println("Downloading file " + filename + "\n")
-	const folder = "./downloadedYaml/"
-	out, err := os.Create(folder + filename)
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-	defer out.Close()
-
-	// Get the data
-	resp, err := http.Get(url + filename)
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-	defer resp.Body.Close()
-
-	// Write the body to file
-	_, err = io.Copy(out, resp.Body)
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-
-	return nil
-}*/
