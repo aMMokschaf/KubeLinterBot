0359c03ac7f479155f4436d3ec99f621ebc72181
Marcel Singer <marcel.singer@hs-bochum.de>
2021-01-19 00:13:54 +0100

    Implemented check for kube-linter-binary

--- internal/callkubelinter/call_kube_linter.go
+++ internal/callkubelinter/call_kube_linter.go
@@ -1,13 +1,15 @@
-//Package callkubelinter executes the KubeLinter-binary.
+//Package callkubelinter checks if the KubeLinter-binary exists, updates and executes it.
 package callkubelinter
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 )
 
 //Callkubelinter calls the kube-linter binary in kubelinter/-folder.
 func Callkubelinter() ([]byte, error) {
+	checkForKubeLinterBinary()
 	//TODO: Change folder?
 	cmd := exec.Command("kubelinter/kube-linter", "lint", "./downloadedYaml/")
 	out, err := cmd.CombinedOutput()
@@ -19,3 +21,15 @@ func Callkubelinter() ([]byte, error) {
 	}
 	return out, err
 }
+
+func checkForKubeLinterBinary() {
+	f, err := os.Open("./kubelinter/kube-linter")
+	if err != nil {
+		fmt.Println("Could not find Kubelinter.", err)
+	} else {
+		fmt.Println("KubeLinter found.")
+	}
+	defer f.Close()
+}
+
+func checkForKubeLinterUpdate() {}
