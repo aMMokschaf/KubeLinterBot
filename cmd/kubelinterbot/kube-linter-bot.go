506483c4700f13e9dc2768c48b8d305a3d1dcfac
Marcel Singer <marcel.singer@hs-bochum.de>
2021-02-01 01:37:38 +0100

    Moved call for kubelinter-binary check to engine. Improved error handling of said function.

--- cmd/kubelinterbot/kube-linter-bot.go
+++ cmd/kubelinterbot/kube-linter-bot.go
@@ -4,6 +4,7 @@ package main
 import (
 	"log"
 	"main/internal/authentication"
+	"main/internal/callkubelinter"
 	"main/internal/config"
 	"main/internal/server"
 	"os"
@@ -12,7 +13,13 @@ import (
 //main sets up a logger, a webHookServer, prints the address and port, starts the server
 func main() {
 	//TODO argument for config file
+	//TODO check if cfg-file exists
 	cfg := config.OptionParser()
+
+	err := callkubelinter.CheckForKubeLinterBinary()
+	if err != nil {
+		//TODO exit server?
+	}
 	//var wg sync.WaitGroup
 	//TODO: implement check if token is actually valid, not just "empty"
 	if cfg.User.AccessToken == "empty" {
