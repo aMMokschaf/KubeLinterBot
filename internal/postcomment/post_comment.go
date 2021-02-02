d21d25e6c87344c3a5bc5e95a225483c1ae7a311
Marcel Singer <marcel.singer@hs-bochum.de>
2021-02-02 04:07:37 +0100

    Little cleanup in postcomment-package

--- internal/postcomment/post_comment.go
+++ internal/postcomment/post_comment.go
@@ -12,8 +12,6 @@ import (
 
 //Push authorizes with github to post KubeLinter's results to the commit.
 func Push(username string, reponame string, commitSha string, result []byte, client *authentication.Client) error {
-	//githubClient := authentication.GetGithubClient()
-
 	var bdy string = string(result)
 	comment := github.RepositoryComment{Body: &bdy}
 	_, _, err := client.GithubClient.Repositories.CreateComment(oauth2.NoContext, username, reponame, commitSha, &comment)
@@ -28,38 +26,30 @@ func Push(username string, reponame string, commitSha string, result []byte, cli
 
 //PullRequestReview TODO blabla
 func PullRequestReview(username string, reponame string, commitSha string, number int, files []string, result []byte, client *authentication.Client) error {
-	fmt.Println("postpullRequestReviewWithComment method")
-	fmt.Println(username, reponame, commitSha, number, files, string(result), client)
-
+	// fmt.Println("postpullRequestReviewWithComment method")
+	// fmt.Println(username, reponame, commitSha, number, files, string(result), client)
 	var comments []*github.DraftReviewComment
-	//build comments
 	for _, file := range files {
 		var commentPath = file
-		var commentPosition = 1 //indicating line 1
-		var commentBody = string(result)
-		fmt.Println("Building comment:\n", commentPath, commentBody, commentPosition)
+		var commentPosition = 1          //indicating line 1
+		var commentBody = string(result) //TODO cleanup: ./downloadedYAML/... should be removed
+		//fmt.Println("Building comment:\n", commentPath, commentBody, commentPosition)
 		var comment = github.DraftReviewComment{}
 		comment.Path = &commentPath
 		comment.Position = &commentPosition
 		comment.Body = &commentBody
 
-		//append comment to array of comments
 		comments = append(comments, &comment)
+		// fmt.Println("New comment built:", comment)
 	}
 
-	fmt.Println(comments)
-
-	//review erstellen
-
 	body := "KubeLinter has found possible security- or production-readiness-errors. Please check the comments made by KubeLinterBot for each file."
-	event := "COMMENT"
+	event := "REQUEST_CHANGES"
 	var review = github.PullRequestReviewRequest{
 		Body:     &body,
 		Event:    &event,
 		Comments: comments,
 	}
-
-	//review abschicken createReview()
 
 	re, resp, err := client.GithubClient.PullRequests.CreateReview(context.Background(), username, reponame, int(number), &review)
 	fmt.Println("create review re", re, "\nresp", resp, "\nerr", err)
